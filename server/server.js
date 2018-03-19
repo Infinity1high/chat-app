84e75b212241134df8dbbca9ec4e77c6bc68f55d
Diana <diana.lisovenko@git.miritec.com>
2018-03-19 09:09:08 +0200

    less4

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 const path = require('path');
+const http = require ('http');
 const express = require('express');
+const socketIO  = require ('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
 const port  = process.env.PORT || 4000;
 
 
 var app = express();
+var server = http.createServer(app ); 
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+io.on('connection',(socket)=> {
+    console.log('new user conncted');
+    socket.emit('newEmail',{
+        email: 'nick@gmail.com'
+
+    });
+    socket.on ('createEmail', (newEmail)=> {
+        console.log('createEmail',newEmail)
+    })
+
+
+    socket.on('disconnect', function ()  {
+        console.log('User was disconnected');
+    
+    })
+} )
+server.listen(port, function () {
     console.log(`Server is up ${port}`);
 
 });
