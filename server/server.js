052e7a8f81edb0ad5d224fe5b14476eb7b8eedf5
developer <infinity1high.dl@gmail.com>
2018-03-19 14:55:49 +0200

    new

--- server/server.js
+++ server/server.js
@@ -14,16 +14,20 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 io.on('connection',(socket)=> {
-    console.log('new user conncted');
-    socket.emit('newEmail',{
-        email: 'nick@gmail.com'
+    console.log('new user connected');
 
-    });
     socket.on ('createEmail', (newEmail)=> {
-        console.log('createEmail',newEmail)
+        console.log('createEmail', newEmail)
+    });
+
+    socket.on ('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from : message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
-
-
     socket.on('disconnect', function ()  {
         console.log('User was disconnected');
     
